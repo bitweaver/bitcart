6412286b77c793a47ec0a436b1caf233c9d18316
spiderr <spider@viovio.com>
2013-04-08 15:08:56 -0400

    fix menu

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,9 +1,20 @@
 <?php
-	global $gBitSystem;
+global $gBitSystem;
 
-	$gBitSystem->registerPackage( 'bitcart', dirname( __FILE__ ).'/' );
 
-	if( $gBitSystem->isPackageActive( 'bitcart' ) ) {
-		$gBitSystem->registerAppMenu( 'bitcart', 'Shopping', BITCART_PKG_URL.'index.php', 'bitpackage:bitcart/menu_bitcart.tpl' );
-	}
+$registerHash = array(
+	'package_name' => 'bitcart',
+	'package_path' => dirname( __FILE__ ).'/',
+	'homeable' => TRUE,
+);
+$gBitSystem->registerPackage( $registerHash );
+
+if( $gBitSystem->isPackageActive( 'bitcart' ) ) {
+	$menuHash = array(
+		'package_name'  => BITCART_PKG_NAME,
+		'index_url'     => BITCART_PKG_URL.'index.php',
+		'menu_template' => 'bitpackage:bitcart/menu_bitcart.tpl',
+	);
+	$gBitSystem->registerAppMenu( $menuHash );
+}
 ?>
